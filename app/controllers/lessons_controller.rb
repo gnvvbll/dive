2bacaaf85910a80f24f1e5021ec2abad8371560f
gnvvbll <gnvvbll@gmail.com>
2020-04-26 17:33:35 -0400

    revamped design of rest of site

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,14 +1,11 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
 
-    def show
-    end
-
-    def require_authorized_for_current_lesson
+  def require_authorized_for_current_lesson
       if current_lesson.user != current_user
         redirect_to course_path, alert: 'You are not enrolled in this course. Enroll to access this page.'
       end
-    end
+  end
 
     private
 
